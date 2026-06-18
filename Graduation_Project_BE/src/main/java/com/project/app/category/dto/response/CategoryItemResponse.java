6e99c2fbd508ad3b10eafe63e36b3aa190c442ca
hoangcow28 <hoanghb2801@gmail.com>
2026-06-18 14:22:17 +0700

    danh mục

---
+++ Graduation_Project_BE/src/main/java/com/project/app/category/dto/response/CategoryItemResponse.java
@@ -0,0 +1,19 @@
+package com.project.app.category.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryItemResponse {
+    private String id;
+    private String label;
+    private String icon;
+    private String color;
+    private String bgColor;
+    private String groupId;
+}
