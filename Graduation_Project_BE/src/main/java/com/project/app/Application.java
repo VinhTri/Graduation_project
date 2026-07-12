4ad7a5bb110a973bbd8b579cac60db84c527a803
Giang120805 <nguyentruonggiang9876543210@gmail.com>
2026-07-13 01:39:19 +0700

    Làm thêm thông báo cho hóa đơn

--- Graduation_Project_BE/src/main/java/com/project/app/Application.java
+++ Graduation_Project_BE/src/main/java/com/project/app/Application.java
@@ -6,9 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableAsync
+@EnableScheduling
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
